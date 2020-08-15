d8c4223dfba2e1b4bef33784ec02b9139edcdecc
Venkataramanan <venkataramanan@Venkataramanans-MacBook-Pro.local>
2020-08-15 18:11:19 +0530

    Changed classes to be more Object Oriented

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -3,31 +3,30 @@ package org.oop;
 interface GenericPoint {
     double x = 0;
     double y = 0;
+
     double distance();
+
     double direction();
 }
+
 public class Point {
-    private double x;
-    private double y;
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
+    private final double x;
+    private final double y;
 
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public double distance(Point to) {
+        double xDistance = this.x - to.x;
+        double yDistance = this.y - to.y;
+        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+    }
+
+    public double direction(Point from) {
+        double xDistance = this.x - from.x;
+        double yDistance = this.y - from.y;
+        return Math.atan2(yDistance, xDistance);
     }
 }
